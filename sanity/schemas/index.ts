726bfba61ad69737a5a8500546e70c6ed35784ff
Rizwan Mansha <104033300+Rizwan-Mansha@users.noreply.github.com>
2023-12-07 20:38:23 +0500

    Fback

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,7 +1,7 @@
-import category from "./category";
-import clothes from "./clothes";
-import Feedback from "./feedback";
-import heroImages from "./heroimages";
-import product from "./product";
+import category from './category'
+import clothes from './clothes'
+import Fback from './feedback'
+import heroImages from './heroimages'
+import product from './product'
 
-export const schemaTypes = [product, category, heroImages, clothes, Feedback]
+export const schemaTypes = [product, category, heroImages, clothes, Fback]
